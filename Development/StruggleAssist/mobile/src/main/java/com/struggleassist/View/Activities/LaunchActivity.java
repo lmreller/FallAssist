a02b0d067b1640627fc664e0983c964d058b3af2
rmcconnon <31744838+rmcconnon@users.noreply.github.com>
2017-09-26 23:53:02 -0500

    Added CreateProfileActivity
    Added Create Profile Activity and xml file

--- Development/StruggleAssist/mobile/src/main/java/com/struggleassist/View/Activities/LaunchActivity.java
+++ Development/StruggleAssist/mobile/src/main/java/com/struggleassist/View/Activities/LaunchActivity.java
@@ -1,9 +1,13 @@
 package com.struggleassist.View.Activities;
 
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.struggleassist.Controller.FallDetection.SensorControllers.AccelerationController;
 import com.struggleassist.Controller.FallDetection.SensorControllers.GravityController;
@@ -18,6 +22,10 @@ public class LaunchActivity extends AppCompatActivity {
     TextView hifi;
     SensorController accel;
     SensorController grav;
+
+    String name;
+    String dateOfBirth;
+    String emergencyContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +46,30 @@ public class LaunchActivity extends AppCompatActivity {
         //register sensors
         accel = new AccelerationController(this);
         accel.start();
+
+        //Create Profile Button
+        Button bCreateProfile = (Button) findViewById(R.id.bCreateProfile);
+        bCreateProfile.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(getApplicationContext(),CreateProfileActivity.class);
+                startActivityForResult(i, 1);
+            }
+        });
+    }
+
+    //Retrieve information from CreateProfile activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK){
+                name = data.getStringExtra("cpName");
+                dateOfBirth = data.getStringExtra("cpDateOfBirth");
+                emergencyContact = data.getStringExtra("cpEmergencyContact");
+
+                Toast.makeText(getApplicationContext(), name + ", " + dateOfBirth + ", " +
+                                emergencyContact, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
