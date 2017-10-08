a12897cc174ef38097d02728303878d59dab68ff
lmreller <31704165+lmreller@users.noreply.github.com>
2017-10-08 15:39:31 -0500

    Created empty Record and User classes in the model
    Record will hold incident records
    User will hold user data

---
+++ Development/StruggleAssist/mobile/src/main/java/com/struggleassist/Model/Record.java
@@ -0,0 +1,9 @@
+package com.struggleassist.Model;
+
+/**
+ * Created by lucas on 10/8/2017.
+ * Data type to hold incident records
+ */
+
+public class Record {
+}
---
+++ Development/StruggleAssist/mobile/src/main/java/com/struggleassist/Model/User.java
@@ -0,0 +1,9 @@
+package com.struggleassist.Model;
+
+/**
+ * Created by lucas on 10/8/2017.
+ * Data type to hold user data
+ */
+
+public class User {
+}
