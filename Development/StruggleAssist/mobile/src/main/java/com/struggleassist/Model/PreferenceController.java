5aa3677ceaec4bdde98c0fe19f0d9639144e0386
lmreller <31704165+lmreller@users.noreply.github.com>
2017-10-08 15:44:16 -0500

    Added empty preference controller to the model
    This class will control the applications access to androids stored preferences

---
+++ Development/StruggleAssist/mobile/src/main/java/com/struggleassist/Model/PreferenceController.java
@@ -0,0 +1,9 @@
+package com.struggleassist.Model;
+
+/**
+ * Created by lucas on 10/8/2017.
+ * Will control the apps access to android stored preferences
+ */
+
+public class PreferenceController {
+}
