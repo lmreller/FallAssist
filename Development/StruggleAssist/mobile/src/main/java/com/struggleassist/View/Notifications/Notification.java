a52d917d1aee8b837a35aeb34723f4ef5c805741
rmcconnon <31744838+rmcconnon@users.noreply.github.com>
2017-10-23 12:01:26 -0500

    Added cancel/confirm buttons
    They don't actually do anything though

--- Development/StruggleAssist/mobile/src/main/java/com/struggleassist/View/Notifications/Notification.java
+++ Development/StruggleAssist/mobile/src/main/java/com/struggleassist/View/Notifications/Notification.java
@@ -1,11 +1,13 @@
 package com.struggleassist.View.Notifications;
 
+import android.app.IntentService;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
 
 import com.struggleassist.Model.ViewContext;
 import com.struggleassist.R;
@@ -28,10 +30,24 @@ public class Notification {
     NotificationCompat.Builder notification;
     private static final int uniqueID = 2112;
 
+    public static final String CANCEL_ACTION = "cancelAction";
+    public static final String CONFIRM_ACTION = "confirmAction";
+
     public void Notify(String notificationTitle, String notificationMessage){
+
+
+        //Cancel intent
+        Intent cancelIntent = new Intent(nContext, NotificationAction.class).setAction(CANCEL_ACTION);
+        PendingIntent cancelPendingIntent = PendingIntent.getService(nContext, 0, cancelIntent, PendingIntent.FLAG_ONE_SHOT);
+        //Confirm intent
+        Intent confirmIntent = new Intent(nContext, NotificationAction.class).setAction(CONFIRM_ACTION);
+        PendingIntent confirmPendingIntent = PendingIntent.getService(nContext, 0, confirmIntent, PendingIntent.FLAG_ONE_SHOT);
 
         notification = new NotificationCompat.Builder(nContext);
         notification.setAutoCancel(true);
+        notification.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+        notification.addAction(R.mipmap.ic_launcher_round,"Cancel",cancelPendingIntent);
+        notification.addAction(R.mipmap.ic_launcher_round,"Confirm",confirmPendingIntent);
 
         //Builds notification
 
@@ -44,12 +60,28 @@ public class Notification {
         notification.setContentText(notificationMessage); //Set notification message
 
         //Brings user to LaunchActivity upon selecting the notification
-        Intent intent = new Intent(nContext, LaunchActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(nContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Intent launchIntent = new Intent(nContext, LaunchActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(nContext, 0, launchIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         notification.setContentIntent(pendingIntent);
 
         //Issues notification
         NotificationManager notificationManager = (NotificationManager) nContext.getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(uniqueID,notification.build());
+    }
+
+    public static class NotificationAction extends IntentService {
+        public NotificationAction(){
+           super(NotificationAction.class.getSimpleName());
+        }
+        @Override
+        protected void onHandleIntent(Intent intent) {
+            String action = intent.getAction();
+            if(CANCEL_ACTION.equals(action)){
+                Toast.makeText(getApplicationContext(),"Canceled",Toast.LENGTH_SHORT);
+            }
+            if(CONFIRM_ACTION.equals(action)){
+                Toast.makeText(getApplicationContext(),"Confirmed",Toast.LENGTH_SHORT);
+            }
+        }
     }
 }
