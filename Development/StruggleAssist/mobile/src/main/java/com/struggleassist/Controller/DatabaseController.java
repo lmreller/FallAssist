5c05df8d554c2d712254cfa478392b822b474ae0
lmreller <31704165+lmreller@users.noreply.github.com>
2017-11-25 18:32:28 -0600

    baseline database class (incomplete)

---
+++ Development/StruggleAssist/mobile/src/main/java/com/struggleassist/Controller/DatabaseController.java
@@ -0,0 +1,112 @@
+package com.struggleassist.Controller;
+
+/**
+ * Created by lucas on 11/25/2017.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseController {
+    //user table
+    static final String KEY_UID = "uid";    //text
+    static final String KEY_FIRSTNAME = "firstName";    //text
+    static final String KEY_LASTNAME = "lastName";  //text
+    static final String KEY_DOB = "dateOfBirth";    //numeric
+    static final String KEY_EMERGENCY = "emergencyContact"; //numeric
+    static final String CREATE_USER = "create table User (uid text not null, firstName text not null, lastName text not null, dateOfBirth numeric not null, emergencyContact numeric not null, PRIMARY KEY (id));";
+
+    //records table
+
+    static final String CREATE_RECORDS = "create table Records (_id integer primary key autoincrement, " + "name text not null, email text not null);";
+
+    static final String TAG = "Database";
+    static final String DATABASE_NAME = "StruggleAssist_DB";
+    static final String DATABASE_TABLE = "contacts";
+    static final int DATABASE_VERSION = 1;
+    final Context context;
+    DatabaseHelper DBHelper;
+    SQLiteDatabase db;
+
+    public DatabaseController(Context ctx) {
+        this.context = ctx;
+        DBHelper = new DatabaseHelper(context);
+    }
+
+    //start
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            try {
+                db.execSQL(CREATE_USER);
+                db.execSQL(CREATE_RECORDS);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS contacts");
+            onCreate(db);
+        }
+    }
+    //end
+
+    //---opens the database--
+    public DatabaseController open() throws SQLException {
+        db = DBHelper.getWritableDatabase();
+        return this;
+    }
+
+    //---closes the database--
+    public void close() {
+        DBHelper.close();
+    }
+
+    //---insert a contact into the database--
+    public long insertContact(String name, String email) {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_NAME, name);
+        initialValues.put(KEY_EMAIL, email);
+        return db.insert(DATABASE_TABLE, null, initialValues);
+    }
+
+    //---deletes a particular contact--
+    public boolean deleteContact(long rowId) {
+        return db.delete(DATABASE_TABLE, KEY_UID + "=" + rowId, null) > 0;
+    }
+
+    //---retrieves all the contacts--
+    public Cursor getAllContacts() {
+        return db.query(DATABASE_TABLE, new String[]{KEY_UID, KEY_NAME, KEY_EMAIL}, null, null, null, null, null);
+    }
+
+    //---retrieves a particular contact--
+    public Cursor getContact(long rowId) throws SQLException {
+        Cursor mCursor = db.query(true, DATABASE_TABLE, new String[]{KEY_UID, KEY_NAME, KEY_EMAIL}, KEY_UID + "=" + rowId, null, null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+    //---updates a contact--
+    public boolean updateContact(long rowId, String name, String email) {
+        ContentValues args = new ContentValues();
+        args.put(KEY_NAME, name);
+        args.put(KEY_EMAIL, email);
+        return db.update(DATABASE_TABLE, args, KEY_UID + "=" + rowId, null) > 0;
+    }
+}
+
