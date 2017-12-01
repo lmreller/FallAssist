7f2675fe18ff6ab21795960306e6ddb50f74f214
lmreller <31704165+lmreller@users.noreply.github.com>
2017-11-30 21:06:45 -0600

    add view profile activity

---
+++ Development/StruggleAssist/mobile/src/main/java/com/struggleassist/View/Activities/Profile/ViewProfileActivity.java
@@ -0,0 +1,15 @@
+package com.struggleassist.View.Activities.Profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.struggleassist.R;
+
+public class ViewProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile);
+    }
+}
