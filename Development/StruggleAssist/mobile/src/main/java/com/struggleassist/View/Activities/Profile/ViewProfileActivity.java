de7edfc150d9e2188f1d511044c2c43386bfa31d
lmreller <31704165+lmreller@users.noreply.github.com>
2017-11-30 21:36:45 -0600

    basic population of view profile

--- Development/StruggleAssist/mobile/src/main/java/com/struggleassist/View/Activities/Profile/ViewProfileActivity.java
+++ Development/StruggleAssist/mobile/src/main/java/com/struggleassist/View/Activities/Profile/ViewProfileActivity.java
@@ -7,9 +7,18 @@ import com.struggleassist.R;
 
 public class ViewProfileActivity extends AppCompatActivity {
 
+    private String fName;
+    private String lName;
+    private String dateOfBirth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
     }
+
+    public void loadUserInfo(){
+
+    }
+
 }
