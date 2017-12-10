8bf271eb62efae3675f928d109122ae17c563c9e
rmcconnon <31744838+rmcconnon@users.noreply.github.com>
2017-12-10 13:56:24 -0600

    Speakerphone
    Speakerphone functionality

--- Development/StruggleAssist/mobile/src/main/java/com/struggleassist/View/Notifications/NotificationReceiver.java
+++ Development/StruggleAssist/mobile/src/main/java/com/struggleassist/View/Notifications/NotificationReceiver.java
@@ -1,20 +1,27 @@
 package com.struggleassist.View.Notifications;
 
+import android.Manifest;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.media.AudioManager;
 import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.support.v4.content.ContextCompat;
+import android.telephony.PhoneStateListener;
 import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
 import android.widget.Toast;
 
 import com.struggleassist.Controller.DatabaseController;
 import com.struggleassist.Model.ViewContext;
 
+import static com.struggleassist.Model.ViewContext.context;
 import static com.struggleassist.Model.ViewContext.getContext;
 
 /**
@@ -40,6 +47,12 @@ public class NotificationReceiver extends BroadcastReceiver {
     private SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(ViewContext.getContext());
     private boolean texts;
     private boolean calls;
+
+
+    TelephonyManager telephonyManager;
+    StatePhoneReceiver phoneStateListener;
+    boolean callFromApp = false;
+    boolean callFromOffHook = false;
 
 
     @Override
@@ -101,9 +114,62 @@ public class NotificationReceiver extends BroadcastReceiver {
     }
 
     public void makeCall(String ecNumber) {
+
+        //Listen for a change in phone calling state
+        phoneStateListener = new StatePhoneReceiver(context);
+        telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+        telephonyManager.listen(phoneStateListener,PhoneStateListener.LISTEN_CALL_STATE);
+        callFromApp = true;
+
         //Make call
         Uri call = Uri.parse("tel:" + ecNumber);
         Intent makeCallIntent = new Intent(Intent.ACTION_CALL, call);
-        getContext().startActivity(makeCallIntent);
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED)
+            getContext().startActivity(makeCallIntent);
+    }
+
+    // Monitor for changes to the state of the phone
+    public class StatePhoneReceiver extends PhoneStateListener {
+        Context context;
+        public StatePhoneReceiver(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            super.onCallStateChanged(state, incomingNumber);
+
+            switch (state) {
+
+                case TelephonyManager.CALL_STATE_OFFHOOK: //Call is established
+                    if (callFromApp) {
+                        callFromApp=false;
+                        callFromOffHook=true;
+
+                        try {
+                            Thread.sleep(500); // Delay 0,5 seconds to handle better turning on loudspeaker
+                        } catch (InterruptedException e) {
+                        }
+
+                        //Activate loudspeaker
+                        AudioManager audioManager = (AudioManager)
+                                context.getSystemService(Context.AUDIO_SERVICE);
+                        audioManager.setMode(AudioManager.MODE_IN_CALL);
+                        audioManager.setSpeakerphoneOn(true);
+                    }
+                    break;
+
+                case TelephonyManager.CALL_STATE_IDLE: //Call is finished
+                    if (callFromOffHook) {
+                        callFromOffHook=false;
+                        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+                        audioManager.setMode(AudioManager.MODE_NORMAL); //Deactivate loudspeaker
+                        telephonyManager.listen(phoneStateListener, // Remove listener
+                                PhoneStateListener.LISTEN_NONE);
+                    }
+                    break;
+            }
+        }
     }
 }
