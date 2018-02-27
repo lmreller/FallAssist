640e661fa29d955129081dee6b58860211d7d74f
rmcconnon <31744838+rmcconnon@users.noreply.github.com>
2018-02-27 11:12:07 -0600

    Full Database Functionality

--- Development/StruggleAssist/mobile/src/main/java/com/struggleassist/Controller/DatabaseController.java
+++ Development/StruggleAssist/mobile/src/main/java/com/struggleassist/Controller/DatabaseController.java
@@ -11,6 +11,10 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.struggleassist.Model.Record;
+import com.struggleassist.Model.ViewContext;
 
 public class DatabaseController {
     //user table
@@ -23,9 +27,20 @@ public class DatabaseController {
     static final String KEY_EMERGENCY_NUMBER = "emergencyContactNumber"; //numeric
     static final String CREATE_USER = "CREATE TABLE IF NOT EXISTS User (uid text, firstName text, lastName text, dateOfBirth text, emergencyContactID text, emergencyContactNumber text, PRIMARY KEY (uid));";
 
-    //records table
+    //Records table
     static final String TABLE_RECORDS = "Records";
-    static final String CREATE_RECORDS = "create table Records (_id integer primary key autoincrement, " + "name text not null, email text not null);";
+    static final String KEY_RID = "rid";
+    static final String KEY_DOI = "dateOfIncident";
+    static final String KEY_LOCATION = "incidentLocation";
+    static final String KEY_VIDEO = "incidentVideo";
+    static final String KEY_NOTES = "incidentNotes";
+    static final String KEY_SCORE = "incidentScore";
+    static final String CREATE_RECORDS = "CREATE TABLE IF NOT EXISTS Records (rid text, dateOfIncident text, incidentLocation text, incidentVideo text, incidentNotes text, incidentScore text, PRIMARY KEY (rid));";
+
+
+//    //records table
+//    static final String TABLE_RECORDS = "Records";
+//    static final String CREATE_RECORDS = "create table Records (_id integer primary key autoincrement, " + "name text not null, email text not null);";
 
     static final String TAG = "Database";
     static final String DATABASE_NAME = "StruggleAssist_DB";
@@ -40,6 +55,7 @@ public class DatabaseController {
         DBHelper = new DatabaseHelper(context);
         open();
         db.execSQL(CREATE_USER);
+        db.execSQL(CREATE_RECORDS);
         close();
     }
 
@@ -52,7 +68,7 @@ public class DatabaseController {
         public void onCreate(SQLiteDatabase db) {
             try {
                 db.execSQL(CREATE_USER);
-                //db.execSQL(CREATE_RECORDS);
+                db.execSQL(CREATE_RECORDS);
                 //db.execSQL(CREATE_HOLDS);
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -78,6 +94,12 @@ public class DatabaseController {
         DBHelper.close();
     }
 
+    //dev purposes only
+    public void reset(){
+        db.execSQL("DROP TABLE "+TABLE_USER);
+        db.execSQL("DROP TABLE"+TABLE_RECORDS);
+    }
+
     public boolean userExists(){
         String count = "SELECT count(*) FROM " + TABLE_USER;
         Cursor mcursor = db.rawQuery(count, null);
@@ -86,11 +108,6 @@ public class DatabaseController {
         boolean exists = icount > 0;
         Log.d("COUNT_USER", Integer.toString(icount));
         return exists;
-    }
-
-    //dev purposes only
-    public void reset(){
-        db.execSQL("DROP TABLE "+TABLE_USER);
     }
 
     //USER OPERATIONS
@@ -138,6 +155,52 @@ public class DatabaseController {
         return db.update(TABLE_USER, args, KEY_UID + "=" + id, null) > 0;
     }
 
+    //---insert record into database
+    public long insertRecord(Record record){
+
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_RID,       record.getId());
+        initialValues.put(KEY_DOI,       record.getDateOfIncident());
+        initialValues.put(KEY_LOCATION,  record.getIncidentLocation());
+        initialValues.put(KEY_VIDEO,     record.getIncidentVideo());
+        initialValues.put(KEY_NOTES,     record.getIncidentNotes());
+        initialValues.put(KEY_SCORE,     Float.toString(record.getIncidentScore()));
+        return db.insert(TABLE_RECORDS, null, initialValues);
+    }
+
+    //---update record
+    public boolean updateRecord(Record record){
+        ContentValues args = new ContentValues();
+        args.put(KEY_RID,       record.getId());
+        args.put(KEY_DOI,       record.getDateOfIncident());
+        args.put(KEY_LOCATION,  record.getIncidentLocation());
+        args.put(KEY_VIDEO,     record.getIncidentVideo());
+        args.put(KEY_NOTES,     record.getIncidentNotes());
+        args.put(KEY_SCORE,     record.getIncidentScore());
+        return db.update(TABLE_RECORDS, args, KEY_RID + "=" + record.getId(), null) > 0;
+    }
+
+    //---remove record
+    public boolean deleteRecord(Record record) {
+        return db.delete(TABLE_RECORDS, KEY_RID + "=" + record.getId(), null) > 0;
+    }
+
+    //---Retrieve all records
+    public Cursor getAllRecords() {
+        return db.query(TABLE_RECORDS, new String[]{KEY_RID, KEY_DOI, KEY_LOCATION, KEY_VIDEO, KEY_NOTES, KEY_SCORE}, null, null, null, null, null);
+    }
+
+    //---Retrieve a particular record
+    public Cursor getRecord(Record record) throws SQLException {
+        Cursor mCursor = db.query(true, TABLE_USER, new String[]{KEY_RID, KEY_DOI, KEY_LOCATION, KEY_VIDEO, KEY_NOTES, KEY_SCORE}, KEY_RID + "=" + record.getId(), null, null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+
+
 /*
 
     //RECORDS OPERATIONS
@@ -179,4 +242,3 @@ public class DatabaseController {
     */
 
 }
-
